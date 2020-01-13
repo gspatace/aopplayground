279853f74f20a1d79162a718b2f4def55a65c50c
George Spatacean <george.spatacean@gmail.com>
2020-01-13 23:13:46 +0200

    Code cleanup

--- aopapp/src/main/java/com/gspatace/playground/aop/app/App.java
+++ aopapp/src/main/java/com/gspatace/playground/aop/app/App.java
@@ -2,10 +2,6 @@ package com.gspatace.playground.aop.app;
 
 import com.gspatace.playground.aop.classes.SomeService;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
--- simpleaspects/src/main/java/com/gspatace/playground/aop/aspects/SimpleAspect.java
+++ simpleaspects/src/main/java/com/gspatace/playground/aop/aspects/SimpleAspect.java
@@ -5,22 +5,16 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
-/**
- * Hello world!
- *
- */
 @Aspect
 public class SimpleAspect
 {
     @Pointcut("execution(* com.gspatace.playground.aop.classes.*.*(..))")
     private void genericPointcut(){}
 
-
     @Before("genericPointcut()")
     public void beforeGenericPointcut() {
         System.out.println("Before");
     }
-
 
     @After("genericPointcut()")
     public void afterGenericPointcut() {
